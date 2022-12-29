5a369f09eb6978fd99cd954d9e53e7a02da74aa2
Julian <mrjrhirst@gmail.com>
2022-12-29 14:33:23 +0000

    Ex4 part 2 complete

--- src/Ex4/ex4.js
+++ src/Ex4/ex4.js
@@ -1,12 +1,14 @@
 const fs = require("fs");
 
 const readInput = (filename) => fs.readFileSync(filename, {encoding: 'utf8', flag:"r"});
-const prepareInput = (input) => input.split('\r\n').map(value => value.split(',').map(value => value.split('-').map(string => +string)));
+const prepareInput = (input) => (input.split('\r\n').map(value => value.split(',').map(value => value.split('-').map(string => +string)))).map(ranges => ranges.map(range => { return {high: range[1], low: range[0]}}));
 
 const input = prepareInput(readInput("./src/ex4/ex4_input.txt"));
 
 console.log(input);
 
-// 532 expected
-console.log(input.reduce((total, value) =>  total + ((((value[0][0] <= value[1][0]) && (value[0][1] >= value[1][1])) || ((value[0][0] >= value[1][0]) && (value[0][1] <= value[1][1]))) ? 1 : 0), 0));
-console.log(input.reduce((total, value) =>  total + ((((value[0][0] >= value[1][0]) && (value[0][0] <= value[1][1])) || ((value[0][1] >= value[1][0]) && (value[0][1] <= value[1][1]))) ? 1 : 0), 0));
+const oneContainsTheOther = (ranges) => (((ranges[0].low <= ranges[1].low) && (ranges[0].high >= ranges[1].high)) || ((ranges[0].low >= ranges[1].low) && (ranges[0].high <= ranges[1].high)));
+const partialIntersect = (ranges) => (((ranges[0].low <= ranges[1].high) && (ranges[0].high >= ranges[1].high)) || ((ranges[0].low <= ranges[1].low) && (ranges[0].high >= ranges[1].low)));
+
+console.log(`Part 1: ${input.reduce((total, ranges) =>  total + (oneContainsTheOther(ranges) ? 1 : 0), 0)}`); // 532 expected
+console.log(`Part 2: ${input.reduce((total, ranges) =>  total + ( oneContainsTheOther(ranges) || partialIntersect(ranges)  ? 1 : 0), 0)}`); // 854
